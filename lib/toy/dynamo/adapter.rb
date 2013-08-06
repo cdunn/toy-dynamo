0e83221f7dff8eaf08ddd3fae1ac6aa21db5ffe5
Alex Chee <alexchee11@gmail.com>
2013-08-06 15:51:34 -0700

    added default :api_version to DynamoDB:Client to V2 date

--- lib/toy/dynamo/adapter.rb
+++ lib/toy/dynamo/adapter.rb
@@ -13,6 +13,7 @@ module Toy
 
         options[:dynamo_db_endpoint] = config[:endpoint] || Toy::Dynamo::Config.endpoint
         options[:dynamo_db_port] = config[:port] || Toy::Dynamo::Config.port
+        options[:api_version] ||= config[:api_version] || '2012-08-10'
         #:dynamo_db_crc_32_check = false
 
         @@default_client ||= AWS::DynamoDB::Client.new(options)
